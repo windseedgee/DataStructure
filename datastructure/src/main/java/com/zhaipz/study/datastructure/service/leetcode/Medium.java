7c48f944e220cc1484bd13be597fea729fd2f027
zhaipuzhuo <936819866@qq.com>
2022-03-02 22:27:30 +0800

    leetcode

--- datastructure/src/main/java/com/zhaipz/study/datastructure/service/leetcode/Medium.java
+++ datastructure/src/main/java/com/zhaipz/study/datastructure/service/leetcode/Medium.java
@@ -137,7 +137,7 @@ public class Medium {
     }
 
     /**
-     * @Title: 33. 搜索旋转排序数组
+     * @Title: 33. 搜索出现的第一个位置和最后一个位置
      */
     public int[] searchRange(int[] nums, int target) {
         int l = search(nums,target);
@@ -303,7 +303,7 @@ public class Medium {
                 }
                 res.add(ints);
             }else {
-                l = Math.max(l,ints[0]);
+                l = Math.min(l,ints[0]);
                 r = Math.max(r,ints[1]);
             }
         }
@@ -312,6 +312,185 @@ public class Medium {
 
         return res.toArray(new int[0][]);
     }
+
+    /**
+     * @Title: 61. 旋转链表
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null)return head;
+        int len = 1;
+        ListNode temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+            len++;
+        }
+        temp.next = head;
+
+        ListNode res = head;
+        for(int i = 0;i < len-1-k%len;i++){
+            res = res.next;
+        }
+        ListNode newHead = res.next;
+        res.next = null;
+
+        return newHead;
+    }
+
+    /**
+     * @Title: 62. 不同路径
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0;i < m;i++){
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for(int i = 1;i < m;i++){
+            for(int j = 1;j < n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+    /**
+     * @Title: 62. 不同路径Ⅱ
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        obstacleGrid[0][0] = obstacleGrid[0][0]==1?0:1;
+        for(int i = 1;i < m;i++){
+            obstacleGrid[i][0] = obstacleGrid[i-1][0]==0?0:obstacleGrid[i][0]==1?0:1;
+        }
+        for(int i = 1;i < n;i++){
+            obstacleGrid[0][i] = obstacleGrid[0][i-1]==0?0:obstacleGrid[0][i]==1?0:1;
+        }
+
+        for(int i = 1;i < m;i++){
+            for(int j = 1;j < n;j++){
+                obstacleGrid[i][j] = obstacleGrid[i][j]==1?0:obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+            }
+        }
+        return obstacleGrid[m-1][n-1];
+    }
+
+    /**
+     * @Title: 73. 矩阵置零
+     */
+    public void setZeroes(int[][] matrix) {
+        boolean flag = false;
+
+        for(int i = 0;i < matrix.length;i++){
+            if(matrix[i][0] == 0 && !flag)flag = true;
+
+            for(int j = 1;j < matrix[0].length;j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = matrix.length-1;i >= 0;i--){
+            for(int j = 1;j < matrix[0].length;j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)matrix[i][j] = 0;
+            }
+            if(flag)matrix[i][0] = 0;
+        }
+    }
+
+    /**
+     * @Title: 74. 搜索二维矩阵
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int[] ma : matrix){
+            if(ma[0] <= target && target <= ma[n-1]){
+                int l = 0,r = n-1;
+                while(l <= r){
+                    int mid = l + (r-l)/2;
+                    if(ma[mid] == target)return true;
+                    if(ma[mid] < target){
+                        l = mid+1;
+                    }else {
+                        r = mid-1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @Title: 75. 颜色分类
+     */
+    public void sortColors(int[] nums) {
+        int p0 = 0,p1 = 0;
+
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] == 1){
+                swap(nums,i,p1);
+                p1++;
+            }
+            if(nums[i] == 0){
+                swap(nums,i,p0);
+                if(p0 < p1){
+                    swap(nums,i,p1);
+                }
+                p0++;
+                p1++;
+            }
+        }
+    }
+
+    /**
+     * @Title: 80. 删除有序数组中的重复项 II
+     */
+    public int removeDuplicates(int[] nums) {
+        int count = 1;
+        int i = 0;
+
+        for(int j = 1;j < nums.length;j++){
+            if(nums[i] == nums[j] && count < 2){
+                count++;
+                i++;
+                nums[i] = nums[j];
+            }else if(nums[i] != nums[j]){
+                count = 1;
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+
+    /**
+     * @Title: 82. 删除排序链表中的重复元素 II
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode res = new ListNode();
+        res.next = head;
+        ListNode temp = res;
+
+        while(temp.next != null && temp.next.next != null){
+            if(temp.next.val == temp.next.next.val){
+                int x = temp.next.val;
+                while(temp.next != null && temp.next.val == x){
+                    temp.next = temp.next.next;
+                }
+            }else temp = temp.next;
+
+        }
+        return res.next;
+    }
+
+    /**
+     * @Title: 92. 反转链表
+     */
+
 
     public static class ListNode {
         int val;
