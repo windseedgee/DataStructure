6071adeba1f2503c76bbfd437d24a9991c68e026
zhaipuzhuo <936819866@qq.com>
2022-03-18 19:51:32 +0800

    leetcode

--- datastructure/src/main/java/com/zhaipz/study/datastructure/service/TestHello.java
+++ datastructure/src/main/java/com/zhaipz/study/datastructure/service/TestHello.java
@@ -81,6 +81,30 @@ public class TestHello {
 //        log.info("main s2 : {}",s2.getAge());
     }
 
+    /**
+     * 根据日期获取 星期 （2019-05-06 ——> 星期一）
+     * @param datetime
+     * @return
+     */
+    public static String dateToWeek(String datetime) {
+
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+        Calendar cal = Calendar.getInstance();
+        Date date;
+        try {
+            date = f.parse(datetime);
+            cal.setTime(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //一周的第几天
+        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+        if (w < 0)
+            w = 0;
+        return weekDays[w];
+    }
+
     public static void swap(Student x,Student y){
         int temp = x.getAge();
         x.setAge(y.getAge());
--- datastructure/src/main/java/com/zhaipz/study/datastructure/service/array/Array.java
+++ datastructure/src/main/java/com/zhaipz/study/datastructure/service/array/Array.java
@@ -1,5 +1,9 @@
 package com.zhaipz.study.datastructure.service.array;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Service;
+
 import javax.xml.soap.Node;
 import java.util.Deque;
 import java.util.LinkedList;
@@ -10,7 +14,8 @@ import java.util.LinkedList;
  * @Description: (这里用一句话描述这个类的作用)
  * @date 2020/12/20 16:26
  */
-public class Array {
+@Service
+public class Array implements BeanPostProcessor {
 
     public static void main(String[] args) {
         System.out.println("xxxx");
@@ -18,4 +23,18 @@ public class Array {
         Node node;
 
     }
+
+//    @Override
+//    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+//        // TODO Auto-generated method stub
+//        System.out.println("postProcessBeforeInitialization..."+beanName+"=>"+bean);
+//        return bean;
+//    }
+//
+//    @Override
+//    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+//        // TODO Auto-generated method stub
+//        System.out.println("postProcessAfterInitialization..."+beanName+"=>"+bean);
+//        return bean;
+//    }
 }
